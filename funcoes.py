06973ef4d8f0957d614b0e96f78f94329c2875ed
haim <haimbm@al.insper.edu.br>
2025-05-02 00:23:11 -0300

    f calcula pontos regra simples

--- funcoes.py
+++ funcoes.py
@@ -11,3 +11,26 @@ def remover_dado(dadosrolados,dadosestoque,dadopararemover):
     dadosrolados.append(dadosestoque[dadopararemover])
     del dadosestoque[dadopararemover]
     return [dadosrolados,dadosestoque]
+
+def calcula_pontos_regra_simples(dadosrolados):
+    s1 = 0
+    s2 = 0
+    s3 = 0
+    s4 = 0
+    s5 = 0
+    s6 = 0
+    for dado in dadosrolados:
+        if dado == 1:
+            s1 += 1
+        elif dado == 2:
+            s2 += 2
+        elif dado == 3:
+            s3 += 3
+        elif dado == 4:
+            s4 += 4
+        elif dado == 5:
+            s5 += 5
+        elif dado == 6:
+            s6 += 6
+    somasimples = {1:s1, 2:s2, 3:s3, 4:s4, 5:s5, 6:s6}
+    return somasimples
