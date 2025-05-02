bbbb732e80d4b1642a237e8f82e3b1fc3eb319d6
haim <haimbm@al.insper.edu.br>
2025-05-02 00:10:49 -0300

    f guardar dado feito

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,8 @@
 def rolar_dados(nd):
     from random import randint
     return [randint(1, 6) for _ in range(nd)]
+
+def guardar_dado(dadosrolados,dadosestoque,dadoparaguardar):
+    dadosestoque.append(dadosrolados[dadoparaguardar])
+    del dadosrolados[dadoparaguardar]
+    return [dadosrolados,dadosestoque]
