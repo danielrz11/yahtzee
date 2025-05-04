0b611fb9b58df9da3b87c4508bcd277e8fef82dc
haim <haimbm@al.insper.edu.br>
2025-05-04 19:17:02 -0300

    quina

--- funcoes.py
+++ funcoes.py
@@ -114,3 +114,33 @@ def calcula_pontos_quadra(dadosrolados):
         return s1*1 + s2*2 + s3*3 + s4*4 + s5*5 + s6*6 
     else:
         return 0
+
+def calcula_pontos_quina(dadosrolados):
+    s1 = 0
+    s2 = 0
+    s3 = 0
+    s4 = 0
+    s5 = 0
+    s6 = 0
+    for dado in dadosrolados:
+        if dado == 1:
+            s1 += 1
+        elif dado == 2:
+            s2 += 1
+        elif dado == 3:
+            s3 += 1
+        elif dado == 4:
+            s4 += 1
+        elif dado == 5:
+            s5 += 1
+        elif dado == 6:
+            s6 += 1
+    listaquantdados = [s1,s2,s3,s4,s5,s6]
+    m = 0
+    for i in listaquantdados:
+        if i > m:
+            m = i
+    if m >= 5:
+        return 50 
+    else:
+        return 0
